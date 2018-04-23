a3d03b9ddecd0e6e1c99bb31bb353b489864b2fa
The Naked Developer <info@thenakeddeveloper.com>
2018-04-24 00:25:03 +0100

    A better home grown Tolerent Enum Parser

--- TNDStudios.Blogs/Helpers/JsonHelpers.cs
+++ TNDStudios.Blogs/Helpers/JsonHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,146 +10,62 @@ using System.Text;
 namespace TNDStudios.Blogs.Helpers
 {
     /// <summary>
-    /// Third part include to get around issue with intollerent Json conversion of enumerations
-    /// https://gist.github.com/gubenkoved/999eb73e227b7063a67a50401578c3a7#file-tolerantenumconverter-cs
+    /// Better implementation of a tolerant enum converter for Newtonsoft Json
     /// </summary>
-    public class TolerantEnumConverter : JsonConverter
+    public class TolerantEnumConverter : StringEnumConverter
     {
-        [ThreadStatic]
-        private Dictionary<Type, Dictionary<string, object>> _fromValueMap; // string representation to Enum value map
-
-        [ThreadStatic]
-        private Dictionary<Type, Dictionary<object, string>> _toValueMap; // Enum value to string map
-
-        public string UnknownValue { get; set; } = "Unknown";
-
-        public override bool CanConvert(Type objectType)
-        {
-            Type type = IsNullableType(objectType) ? Nullable.GetUnderlyingType(objectType) : objectType;
-            return type.IsEnum;
-        }
-
+        /// <summary>
+        /// Override of the base Json reader with an error trap to handle default handling
+        /// </summary>
+        /// <param name="reader">Passthrough</param>
+        /// <param name="objectType">Passthrough</param>
+        /// <param name="existingValue">Passthrough</param>
+        /// <param name="serializer">Passthrough</param>
+        /// <returns>The enum value</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            bool isNullable = IsNullableType(objectType);
-            Type enumType = isNullable ? Nullable.GetUnderlyingType(objectType) : objectType;
-
-            InitMap(enumType);
-
-            if (reader.TokenType == JsonToken.String)
+            try
             {
-                string enumText = reader.Value.ToString();
-
-                object val = FromValue(enumType, enumText);
-
-                if (val != null)
-                    return val;
+                // Try and do the transformation with the base implementation
+                return base.ReadJson(reader, objectType, existingValue, serializer);
             }
-            else if (reader.TokenType == JsonToken.Integer)
+            catch(Exception ex)
             {
-                int enumVal = Convert.ToInt32(reader.Value);
-                int[] values = (int[])Enum.GetValues(enumType);
-                if (values.Contains(enumVal))
+                // A default not defined for this enum? Do some extra work ..
+                if (existingValue == null)
                 {
-                    return Enum.Parse(enumType, enumVal.ToString());
-                }
-            }
+                    // No default, go try and get the first value
+                    object returnValue = GetFirstEnumValue(objectType); 
 
-            if (!isNullable)
-            {
-                string[] names = Enum.GetNames(enumType);
+                    // Still could not resolve a value so throw the origional error
+                    if (returnValue == null)
+                        throw ex;
 
-                string unknownName = names
-                    .Where(n => string.Equals(n, UnknownValue, StringComparison.OrdinalIgnoreCase))
-                    .FirstOrDefault();
-
-                if (unknownName == null)
-                {
-                    throw new JsonSerializationException($"Unable to parse '{reader.Value}' to enum {enumType}. Consider adding Unknown as fail-back value.");
-                }
-
-                return Enum.Parse(enumType, unknownName);
-            }
-
-            return null;
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            Type enumType = value.GetType();
-
-            InitMap(enumType);
-
-            string val = ToValue(enumType, value);
-
-            writer.WriteValue(val);
-        }
-
-        #region Private methods
-        private bool IsNullableType(Type t)
-        {
-            return (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>));
-        }
-
-        private void InitMap(Type enumType)
-        {
-            if (_fromValueMap == null)
-                _fromValueMap = new Dictionary<Type, Dictionary<string, object>>();
-
-            if (_toValueMap == null)
-                _toValueMap = new Dictionary<Type, Dictionary<object, string>>();
-
-            if (_fromValueMap.ContainsKey(enumType))
-                return; // already initialized
-
-            Dictionary<string, object> fromMap = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
-            Dictionary<object, string> toMap = new Dictionary<object, string>();
-
-            FieldInfo[] fields = enumType.GetFields(BindingFlags.Static | BindingFlags.Public);
-
-            foreach (FieldInfo field in fields)
-            {
-                string name = field.Name;
-                object enumValue = Enum.Parse(enumType, name);
-
-                // use EnumMember attribute if exists
-                EnumMemberAttribute enumMemberAttrbiute = field.GetCustomAttribute<EnumMemberAttribute>();
-
-                if (enumMemberAttrbiute != null)
-                {
-                    string enumMemberValue = enumMemberAttrbiute.Value;
-
-                    fromMap[enumMemberValue] = enumValue;
-                    toMap[enumValue] = enumMemberValue;
+                    // Made it to the end, return the value found
+                    return returnValue;
                 }
                 else
-                {
-                    toMap[enumValue] = name;
-                }
-
-                fromMap[name] = enumValue;
+                    return existingValue; // We have a default value, just return that ..
             }
-
-            _fromValueMap[enumType] = fromMap;
-            _toValueMap[enumType] = toMap;
         }
 
-        private string ToValue(Type enumType, object obj)
+        /// <summary>
+        /// Get the first value of the enumeration (used for when an existing default value hasn't been defined)
+        /// </summary>
+        /// <param name="enumType">The type of enum proposed</param>
+        /// <returns>The first enum value</returns>
+        private object GetFirstEnumValue(Type enumType)
         {
-            Dictionary<object, string> map = _toValueMap[enumType];
-
-            return map[obj];
-        }
-
-        private object FromValue(Type enumType, string value)
-        {
-            Dictionary<string, object> map = _fromValueMap[enumType];
-
-            if (!map.ContainsKey(value))
+            try
+            {
+                // Attempt to get the first of the values of this enum
+                return Enum.GetValues(enumType).GetValue(0);
+            }
+            catch
+            {
+                // Throw the error away and return nothing (which will be handled)
                 return null;
-
-            return map[value];
+            }
         }
-        #endregion
     }
 }
