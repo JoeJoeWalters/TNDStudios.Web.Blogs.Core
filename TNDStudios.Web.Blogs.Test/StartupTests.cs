058831cd7360e036866b3c3fef51c39eee31e15f
The Naked Developer <info@thenakeddeveloper.com>
2018-08-20 13:28:42 +0100

    Refactoring of the attribute discovery on blogs to facilitate the testing of the startup / discovery functionality (Issue #9) .. Plus it just makes more sense ..

---
+++ TNDStudios.Web.Blogs.Test/StartupTests.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using TNDStudios.Web.Blogs.Core.Providers;
+using TNDStudios.Web.Blogs.Core.RequestResponse;
+using Xunit;
+
+namespace TNDStudios.Web.Blogs.Core.Test
+{
+    /// <summary>
+    /// Setup fixture for the tests
+    /// </summary>
+    public class StartupTestsFixture : IDisposable
+    {
+        /// <summary>
+        /// Setup parameters for this set of tests
+        /// </summary>
+
+        /// <summary>
+        /// Setup the fixture with the needed items
+        /// </summary>
+        public StartupTestsFixture()
+        {
+        }
+
+        /// <summary>
+        /// Implementation of IDisposable
+        /// </summary>
+        public void Dispose()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Test runner implementing the appropriate fixture
+    /// </summary>
+    public class StartupTests : IClassFixture<StartupTestsFixture>, IDisposable
+    {
+        /// <summary>
+        /// Holder for the fixture data
+        /// </summary>
+        private StartupTestsFixture fixture;
+
+        /// <summary>
+        /// Constructor with the fixture (setup) passed in to it
+        /// </summary>
+        /// <param name="data"></param>
+        public StartupTests(StartupTestsFixture data)
+        {
+            fixture = data; // Assign the instantiated fixture data locally
+        }
+
+        [Fact(DisplayName = "Blog Startup - Initialise Blog Controller")]
+        public void Initialise_New_BlogController()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        
+        /// <summary>
+        /// Implementation of IDisposable
+        /// </summary>
+        public void Dispose()
+        {
+        }
+    }
+}
--- TNDStudios.Web.Blogs/Helpers/Startup/BlogRegistrationHelper.cs
+++ TNDStudios.Web.Blogs/Helpers/Startup/BlogRegistrationHelper.cs
@@ -14,43 +14,73 @@ namespace TNDStudios.Web.Blogs.Core.Helpers
     public class BlogRegistrationHelper
     {
         /// <summary>
-        /// Register a controller based on the instance type
+        /// Wrapper for registering a controller around the testable functionality
+        /// as the controller discovery cannot be tested within the context of the tests
         /// </summary>
         /// <param name="instanceType">The new blog controller (or type) to be registered</param>
-        /// <param name="Blogs">The list of blogs to register against (strictly ref isn't needed but added to make clear)</param>
+        /// <param name="blogs">The list of blogs to register against (strictly ref isn't needed but added to make clear)</param>
         public Boolean Register(Type instanceType, ref Dictionary<String, IBlog> blogs)
         {
             // The result which is failed by default
             Boolean result = false;
 
+            try
+            {
+                // Try and scan the attributes from the blog controller instance
+                BlogSetupAttribute[] setupAttributes = (BlogSetupAttribute[])instanceType.GetCustomAttributes(typeof(BlogSetupAttribute), false);
+                BlogSEOAttribute[] seoAttributes = (BlogSEOAttribute[])instanceType.GetCustomAttributes(typeof(BlogSEOAttribute), false);
+
+                // Call the testable functionality from this wrapper with the derived attributes
+                result = Register(instanceType.Name, setupAttributes, seoAttributes, ref blogs);
+            }
+            catch (Exception ex)
+            {
+                // Tell the caller we could not initialise the blog controller
+                throw BlogException.Passthrough(ex, new NotInitialisedBlogException(ex));
+            }
+
+            return result; // Send back the result
+        }
+
+        /// <summary>
+        /// Overload to take the setup attributes and blog id so that tests can 
+        /// be ran against them without needing it scan for the blog controller
+        /// </summary>
+        /// <returns></returns>
+        public Boolean Register(String instanceName,
+            BlogSetupAttribute[] setupAttributes, 
+            BlogSEOAttribute[] seoAttributes, 
+            ref Dictionary<String, IBlog> blogs)
+        {
+            // Define the result as failed by default
+            Boolean result = false;
+
             // Check to see if the blogs dictionary has been instantiated (is not from the startup routine)
             blogs = blogs ?? new Dictionary<string, IBlog>();
-
+            
             // If the base controller hasn't been set up yet the go gather the information it
             // needs to connect to the blogs that have been assigned for it to manage
-            if (!blogs.ContainsKey(instanceType.Name))
+            if (!blogs.ContainsKey(instanceName))
             {
                 try
                 {
                     // Get all of the blog setup attributes for this controller / blog combination
-                    BlogSetupAttribute[] attrs = (BlogSetupAttribute[])instanceType.GetCustomAttributes(typeof(BlogSetupAttribute), false);
-                    if (attrs.Length > 0)
+                    if (setupAttributes.Length > 0)
                     {
                         // Get the blog Id from the calling class custom parameters
-                        String blogId = attrs[0].BlogId;
+                        String blogId = setupAttributes[0].BlogId;
 
                         // Work out and create an instance of the correct data provider
-                        IBlogDataProvider blogDataProvider = (new BlogDataProviderFactory()).Get(attrs[0].Provider);
+                        IBlogDataProvider blogDataProvider = (new BlogDataProviderFactory()).Get(setupAttributes[0].Provider);
                         if (blogDataProvider != null)
                         {
-                            blogDataProvider.ConnectionString = new BlogDataProviderConnectionString(attrs[0].ProviderConnectionString);
+                            blogDataProvider.ConnectionString = new BlogDataProviderConnectionString(setupAttributes[0].ProviderConnectionString);
 
                             // Initialise the data provider
                             blogDataProvider.Initialise();
 
                             // Get all of the blog setup attributes for this controller / blog combination
-                            BlogSEOAttribute[] sEOAttrs = (BlogSEOAttribute[])instanceType.GetCustomAttributes(typeof(BlogSEOAttribute), false);
-                            BlogSEOSettings blogSEOSettings = (sEOAttrs.Length > 0) ? sEOAttrs[0].SEOSettings : new BlogSEOSettings() { };
+                            BlogSEOSettings blogSEOSettings = (seoAttributes.Length > 0) ? seoAttributes[0].SEOSettings : new BlogSEOSettings() { };
 
                             // Construct the parameters for setting up the blog
                             IBlogParameters blogParameters = new BlogParameters()
@@ -61,7 +91,7 @@ namespace TNDStudios.Web.Blogs.Core.Helpers
                             };
 
                             // Assign the instantiated blog class to the static array of blogs
-                            blogs[instanceType.Name] = new Blog(blogParameters);
+                            blogs[instanceName] = new Blog(blogParameters);
 
                             // Success
                             result = true;
@@ -75,7 +105,8 @@ namespace TNDStudios.Web.Blogs.Core.Helpers
                 }
             }
 
-            return result; // Send back the result
+            // Return if it was successful
+            return result;
         }
     }
 }
